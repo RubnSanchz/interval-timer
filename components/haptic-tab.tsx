8c626a1ea45a517bc2d8f8d8c6ef0feaf84899ee
Rubén Sánchez <rsanchezsd@gmail.com>
2026-02-21 01:02:34 +0100

    feat: add sound when set ends

--- components/haptic-tab.tsx
+++ components/haptic-tab.tsx
@@ -1,13 +1,17 @@
 import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
 import { PlatformPressable } from '@react-navigation/elements';
 import * as Haptics from 'expo-haptics';
+import { useAppSettingsOptional } from '@/hooks/use-app-settings';
 
 export function HapticTab(props: BottomTabBarButtonProps) {
+  const settings = useAppSettingsOptional();
+  const hapticsEnabled = settings?.hapticsEnabled ?? true;
+
   return (
     <PlatformPressable
       {...props}
       onPressIn={(ev) => {
-        if (process.env.EXPO_OS === 'ios') {
+        if (process.env.EXPO_OS === 'ios' && hapticsEnabled) {
           // Add a soft haptic feedback when pressing down on the tabs.
           Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
         }
